20f1526fd13052e2305951873bac777224c4f25b
Mateusz Pomagier <mpomagier@gmail.com>
2023-11-29 13:01:57 +0100

    Init project & first component approach in React

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,12 @@
+import styles from "./SearchForm.module.scss";
+import TextInput from "../TextInput/TextInput";
+
+const SearchForm = () => {
+  return (
+    <form className={styles.searchForm}>
+      <TextInput placeholder="Search..." />
+      <button className={styles.button}>Search</button>
+    </form>
+  );
+};
+export default SearchForm;
